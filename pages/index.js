69b70b253b926b25dec186279c54ef6b1fdbd6c3
0xEddie <eomeasoo@ualberta.ca>
2022-12-05 10:49:24 -0700

    push

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,29 @@ const Home = () => {
   const onUserChangedText = (event) => {
     setUserInput(event.target.value);
   };
+  
+  const [apiOutput, setApiOutput] = useState('');
+  const [isGenerating, setIsGenerating] = useState(false);
+  
+  const callGenerateEndpoint = async() => {
+    setIsGenerating(true);
+    
+    console.log("Calling OpenAI...");
+    const response = await fetch('/api/generate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ userInput }),
+    });
+    
+    const data = await response.json();
+    const { output } = data;
+    console.log("OpenAI replied...", output.text);
+  
+    setApiOutput(`${output.text}`);
+    setIsGenerating(false);
+  };
   return (
     <div className="root">
       <Head>
@@ -19,7 +42,7 @@ const Home = () => {
             <h1>Ask Alan Watts anything!</h1>
           </div>
           <div className="header-subtitle">
-            <h2>Alan Watts knows a lot about Buddhism and Eastern philosophy. Do you have any questions for him?</h2>
+            <h2>Alan Watts bot knows a lot about Buddhism and Eastern philosophy. Do you have any questions for him?</h2>
           </div>
         </div>
         <div className='prompt-container'>
@@ -30,7 +53,7 @@ const Home = () => {
             onChange={onUserChangedText}
           />;
         <div className='prompt-buttons'>
-          <a className='generate-button' onClick={null}>
+          <a className='generate-button' onClick={callGenerateEndpoint}>
             <div className='generate'>
               <p>Generate</p>
             </div>
