faf695272baede2483539a88ed063b4a44c84efe
0xEddie <eomeasoo@ualberta.ca>
2022-12-05 13:28:37 -0700

    tidy output

--- pages/index.js
+++ pages/index.js
@@ -56,9 +56,12 @@ const Home = () => {
           />;
 
           <div className='prompt-buttons'>
-            <a className='generate-button' onClick={callGenerateEndpoint}>
+            <a
+              className={isGenerating ? 'generate-button loading' : 'generate-button'} 
+              onClick={callGenerateEndpoint}
+            >
               <div className='generate'>
-                <p>Generate</p>
+                {isGenerating ? <span className='loader'></span> : <p>Generate</p>}
               </div>
             </a>
           </div>
@@ -67,7 +70,7 @@ const Home = () => {
             <div className='output'>
               <div className='output-header-container'>
                 <div className='output-header'>
-                  <h3>Output</h3>
+                  <h3>Alan says:</h3>
                 </div>
               </div>
               <div className='output-content'>
